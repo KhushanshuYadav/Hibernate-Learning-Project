a0b1e03862c5785874af8dfbf61fd02c13e58820
Khushanshu Yadav <khushanshuyadav2@gmail.com>
2025-02-21 22:08:20 +0530

    3. Created the class for the data we want to store in DB

---
+++ src/main/java/com/khushanshu/Student.java
@@ -0,0 +1,45 @@
+package com.khushanshu;
+
+//this is our class which will hold the data in java code
+//Now we will store this data in database using hibernate
+
+
+public class Student {
+
+    int sId;
+    String sName;
+    int sAge;
+
+    public void setsId(int sId) {
+        this.sId = sId;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public void setsAge(int sAge) {
+        this.sAge = sAge;
+    }
+
+    public int getsId() {
+        return sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public int getsAge() {
+        return sAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sId=" + sId +
+                ", sName='" + sName + '\'' +
+                ", sAge=" + sAge +
+                '}';
+    }
+}
